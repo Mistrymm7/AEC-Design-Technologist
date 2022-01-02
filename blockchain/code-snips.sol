4b9b0b553ab5b6b0a99f71e770f554eea0425e95
Mayur Mistry <mmistry2@illinois.edu>
2022-01-01 22:36:21 -0600

    All Key Components

--- blockchain/code-snips.sol
+++ blockchain/code-snips.sol
@@ -8,9 +8,15 @@ contract MM{
     string starter = "ArchiDAO to the Moon";
     address account = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
+    // construction
+    address owner;
+    constructor() payable{
+    owner = msg.sender;
+    }
+
     // array
-    string[] public dao = ["awesome", "cutting edge", "radical"];
-    uint256[] public projectbudget = [1000,25000, 3500];
+    string[] dao = ["awesome", "cutting edge", "radical"];
+    uint256[] projectbudget = [1000,25000, 3500];
 
     // struct - similar to class
     struct Persondata {
@@ -21,20 +27,63 @@ contract MM{
     }
 
     // instantiate
-    Persondata public mayur;
-    Persondata public nondefault;
+    Persondata mayur;
+    Persondata nondefault;
 
     // define values
-    function queryinfo(uint256 _id) public{
+    function queryinfo(uint256 _id) private {
         if (_id== 0){
             mayur = Persondata({firstName:"Mayur", lastName:"Mistry", age:26,city:"Chicago"});
         } else {
             nondefault = Persondata({firstName:"ABC", lastName:"XYZ", age:29,city:"Mars"});
         }
-        
+    }
+
+    //mapping
+    mapping(address => uint)  balanceacc;
+    function checkBalance() private{
+        balanceacc[account] = 50;
+    }
+
+    // not working uint public senderBalance = msg.value; 
+
+    //conditions
+    function changeOwner (address newHolder) private {
+        require(msg.sender == owner, "Only owner can change");
+        owner = newHolder; 
+       
 
     }
 
+    //Check balance on address of smart contract
+    function contractBalance() public view returns (uint){
+        return(address(this).balance);
+    }
 
+    //events and modifiers
+    event userinfo(address _user, uint _amount);
+
+    modifier oneEther(){
+        require(msg.value == 100 wei);
+        _;
+    }
+
+    function sendminEth() public payable oneEther(){
+        emit userinfo(msg.sender, msg.value);
+    }
+
+    //memory (variables are forgotten,saves in gas cost) and storage (replaces the original allocated array)
+
+    uint[] public ogarray;
+
+    function changeValue() public {
+
+        ogarray.push(5);
+        ogarray.push(8);
+
+        // uint[] storage copiedarray = ogarray; (Replaces) Holds state variables and doesnt go away
+        uint[] memory copiedarray = ogarray; 
+        copiedarray[0] = 2;
+    }
 
 }
