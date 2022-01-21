1735b6dc111ef51880448757feca854080e5261d
Mayur Mistry <mmistry2@illinois.edu>
2022-01-21 06:38:14 -0600

    html dom imp utils

---
+++ web_dev/html_dom_script_tag_chrome_dev.js
@@ -0,0 +1,44 @@
+
+//Create text DOM from console/script tag
+var img = new Image();
+img.src = 'https://cloud-01.isotile.com/basic/avatars/png/8680.png';
+
+console.log(img.src)
+img.width=250;
+document.getElementById('id').appendChild(img);
+
+
+ //Editing Html directly from console
+document.bgColor="red"
+const para = document.createElement('p')
+const node = document.createTextNode("This is cool")
+para.appendChild(node)
+const element =document.getElementById("div1")
+
+//Create text DOM from console/script tag
+const element =document.querySelector('h1')
+element.appendChild(para)
+
+//Create img DOM from console/script tag
+const img = document.createElement("img")
+img.src = mm[0].image.image
+img.width=500
+document.body.appendChild(img);
+
+//Making Fetch Calls
+let res = '';
+fetch('https://www.crazycrows.club/api/4933',{mode:'cors'})
+    .then( response => response.json() )
+    .then( data => {
+        console.log(data); 
+        res=data ;
+    } )
+
+//forEach Array
+nftarrary.forEach((element, index) => {
+tokenss.push(element.token_id);
+
+//Map function
+let mm = ress.map(o => ({token:o.token_id, image:JSON.parse(o.metadata)}))
+let imgurl = [];
+for (let i=0; i<7;i++){ imgurl[i] = mm[i].image.image; console.log(imgurl);}
